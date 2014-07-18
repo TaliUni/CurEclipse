ebae9600c7d2c64cd5b7a3167523f8a2c4f1eebf
TaliUni <tsb4@hw.ac.uk>
2014-07-18 14:56:48 +0100

    Build4 taken from Build1.  Starting point

---
+++ Build4/src/overview/Manager.java
@@ -0,0 +1,18 @@
+package overview;
+
+public class Manager {
+    
+    
+    
+    Build4 buildMy;
+    
+    public Manager()
+    {
+        
+    }
+    
+    public void run()
+    {
+       buildMy = new Build4(); 
+    }
+}
