7dc24c4439e0b45c8b45b1ec3b4011434a01011f
TaliUni <tsb4@hw.ac.uk>
2014-07-30 16:04:45 +0100

    trying to get popup menus with single click.  Not working, need to change direction.  Now need to create own class for equiv popup menu, that doesn't steal the focus, and disallow click to continue to 2 clicks

---
+++ Build5_2/src/overview/Manager.java
@@ -0,0 +1,20 @@
+package overview;
+
+public class Manager {
+    
+	
+    Build5_2 buildMy5_1;
+    
+    public Manager()
+    {
+        
+    }
+    
+    public void run()
+    {
+   
+    	
+    	buildMy5_1 = new Build5_2();
+    	buildMy5_1.createAndShowGUI();
+    }
+}
---
+++ Build5_2/src/overview/Subject.java
@@ -0,0 +1,9 @@
+package overview;
+
+public interface Subject {
+	
+	public void registerObserver(Observer obs);
+	public void removeObserver(Observer obs);
+	public void notifyObservers();
+
+}
